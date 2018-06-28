afe4793f340011a9e12932cbe67780ab126d4ca2
vikram1444 <31198839+vikram1444@users.noreply.github.com>
2018-06-28 16:30:21 +0530

    Update find_in_ordered_Set.py

--- Week 1/Day 4/find_in_ordered_Set.py
+++ Week 1/Day 4/find_in_ordered_Set.py
@@ -1,1 +1,56 @@
+import unittest
+def contains(ordered_list, number): 
+    r = len(ordered_list)-1
+    l = 0
+    while r >= l: 
+        mid = int (l + (r - l)/2)
+        if ordered_list[mid] == number:
+            return True
+        elif ordered_list[mid] > number:
+            r = mid-1
+        else:
+            l = mid+1    
+    return False
 
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_empty_list(self):
+        result = contains([], 1)
+        self.assertFalse(result)
+
+    def test_one_item_list_number_present(self):
+        result = contains([1], 1)
+        self.assertTrue(result)
+
+    def test_one_item_list_number_absent(self):
+        result = contains([1], 2)
+        self.assertFalse(result)
+
+    def test_small_list_number_present(self):
+        result = contains([2, 4, 6], 4)
+        self.assertTrue(result)
+
+    def test_small_list_number_absent(self):
+        result = contains([2, 4, 6], 5)
+        self.assertFalse(result)
+
+    def test_large_list_number_present(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)
+        self.assertTrue(result)
+
+    def test_large_list_number_absent(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 0)
+        self.assertFalse(result)
+
+    def test_large_list_number_first(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1)
+        self.assertTrue(result)
+
+    def test_large_list_number_last(self):
+        result = contains([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
