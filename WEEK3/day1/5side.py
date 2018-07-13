765236f4de4c0e0478de069d51a9ba7990f46846
vikram1444 <31198839+vikram1444@users.noreply.github.com>
2018-07-13 14:42:08 +0530

    Update 5side.py

--- WEEK3/day1/5side.py
+++ WEEK3/day1/5side.py
@@ -1,1 +1,12 @@
+import random
 
+def rand7():
+    return random.randint(1, 7)
+
+def rand5():
+
+    # Implement rand5() using rand7()
+    roll = rand7()
+    return roll if roll <= 5 else rand5()
+print 'Rolling 5-sided die...'
+print rand5()
